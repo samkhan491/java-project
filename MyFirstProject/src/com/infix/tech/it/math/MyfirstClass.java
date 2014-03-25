49e4d55897de079bdff653ebe62b3a5efa1b58ae
samkhan491 <samkhan491@gmail.com>
2014-03-25 12:49:49 -0400

    java code

---
+++ MyFirstProject/src/com/infix/tech/it/math/MyfirstClass.java
@@ -0,0 +1,12 @@
+package com.infix.tech.it.math;
+
+public class MyfirstClass {
+
+    public static void main(String [] args){
+           System.out.println("Reaz Patwary");
+           System.out.print("Reaz Patwary");
+    
+    }
+    
+    
+}    
---
+++ MyFirstProject/src/com/infix/txt/addition/Addition.java
@@ -0,0 +1,20 @@
+package com.infix.txt.addition;
+
+public class Addition {
+    public static void main(String [] args){
+
+    	
+    	
+    	int sF,aK,sM,res;
+    	
+    	sF=90;
+    	aK=50;
+    	res=sF+aK;
+    	
+    	System.out.println("My result is : "+res );
+    			
+             }
+
+
+}
+
