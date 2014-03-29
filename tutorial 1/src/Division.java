29584bff31dd1302cdeda185fb4b0d5903988810
samkhan491 <samkhan491@gmail.com>
2014-03-28 20:38:36 -0400

    java

---
+++ tutorial 1/src/Division.java
@@ -0,0 +1,16 @@
+
+public class Division {
+
+	public static void main(String[] args) {
+		
+		int fN,sK,res;
+		
+		fN=100;
+		sK=10;
+		res=fN/sK;
+		
+		System.out.println("my result is : "+res );
+
+	}
+
+}
---
+++ tutorial 1/src/multiplication.java
@@ -0,0 +1,17 @@
+
+public class multiplication {
+
+	public static void main(String[]args){
+  
+		int fG,aH,aK,res;
+		
+		fG=25;
+		aH=10;
+		aK=15;
+		
+		res=fG*aH*aK;
+		
+		System.out.println("my result is :" +res);
+	
+	}
+}
