dab536e515281681c1f5fc7adbdd36fa1df5bfb0
samkhan491 <samkhan491@gmail.com>
2014-04-11 13:32:30 -0400

    no message

---
+++ Array/src/com/Arrays/ForLoop.java
@@ -0,0 +1,13 @@
+package com.Arrays;
+
+public class ForLoop {
+
+	public static void main(String[] args) {
+		
+		for (int number =5; number<=21;number+=3){
+
+			System.out.println(number);
+	}
+
+}
+}
