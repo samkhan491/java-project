48d45e71426a520522c5f1394ede14b843286979
samkhan491 <samkhan491@gmail.com>
2014-05-01 11:27:25 -0400

    New updates

---
+++ SamKhan/src/com/saddam/IfElsePrac.java
@@ -0,0 +1,24 @@
+package com.saddam;
+
+public class IfElsePrac {
+
+	public static void main(String[] args) {
+		
+		int mark =90;
+		
+		if (mark>=90){
+			System.out.println("A grade");
+			
+		}
+         
+		else if (mark<90){
+			System.out.println("B grade");
+		}
+		else if (mark< 80){
+			System.out.println("C grade");
+		}
+	}
+	
+	
+
+}
---
+++ SamKhan/src/com/saddam/KhanSaddam.java
@@ -0,0 +1,15 @@
+package com.saddam;
+
+public class KhanSaddam {
+
+	public static void main(String[] args) {
+		
+		int a=1;
+		int b=2;
+		
+		 int c= a+b;
+		 
+
+	 System.out.println("result is: "+ c);
+}
+}
